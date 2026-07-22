fa3dc0e27588b24568a0244dc2ee8f8ed03d7da4
sulthann1 <sulthanaawaliya155@gmail.com>
2026-07-22 11:32:58 +0700

    menambahkan routes ke index.js

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const PORT = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/api', require('./routes/api'));
+
 async function startServer() {
     await connectToDatabase();
     app.listen(PORT, () => {
