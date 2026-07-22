6f1fd4f7fa6c2010ee16da84a18e6023e765cfb7
sulthann1 <sulthanaawaliya155@gmail.com>
2026-07-22 11:29:07 +0700

    menambahkan get

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const komikController = require("../controller/komikController");
+
+
+router.get("/komik", komikController.getAllKomik);
